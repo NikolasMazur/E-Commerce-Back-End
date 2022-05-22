8b528a9be048925ab24d38884042981e8b020d82
Nikolas Mazur <kolemazur@gmail.com>
2022-05-21 22:51:25 -0600

    Add delete functions

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -82,6 +82,23 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories were found.' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
